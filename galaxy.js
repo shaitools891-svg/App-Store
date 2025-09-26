a178ad804462a800a130a1a57a888069836b4657
shaitools891-svg <shaitools891@gmail.com>
2025-09-26 09:24:00 +0600

    Increase galaxy animation visibility by boosting density, glow, saturation, and star size

--- galaxy.js
+++ galaxy.js
@@ -48,7 +48,7 @@ window.initGalaxy = function(container, options = {}) {
   geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
 
   const material = new THREE.PointsMaterial({
-    size: 0.1,
+    size: 1,
     vertexColors: true,
     transparent: true,
     opacity: glowIntensity,
